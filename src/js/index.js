287a132b2c3c877739ab8b489d8c6f776fb1f972
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2023-08-05 22:41:54 -0300

    import all services, objects & validations

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import { serviceUser } from "./services/user.js";
+import { serviceRepositories } from "./services/repos.js";
+
+import { objectUser as user } from "./objects/user.js";
+import { screen } from "./objects/screen.js";
+
+import { isEmpty } from "./validations.js";
+import { notFound } from "./validations.js";
